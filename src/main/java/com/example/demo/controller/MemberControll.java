f7a7ce7cdc10c773eceb4c9143a77383d87e21a7
kwanhyunkim <molba06@naver.com>
2023-11-08 20:39:43 +0900

    회원가입 : FORM 제작

---
+++ src/main/java/com/example/demo/controller/MemberControll.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Member;
+import com.example.demo.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberControll {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Member member = new Member();
+        member.setUsername(form.getUsername());
+        member.setEmail(form.getEmail());
+        member.setBirthday(form.getBirthday());
+        member.setPassword(form.getPassword());
+        member.setPhonenumber(form.getPhonenumber());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+}
---
+++ src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,27 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name ="member_id")
+    private Long id;
+
+    private String email; // 이메일
+
+    private String username;
+
+    private String birthday;
+
+    private String password;
+
+    private String phonenumber;
+}
