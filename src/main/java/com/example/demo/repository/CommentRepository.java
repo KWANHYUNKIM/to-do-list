bd7bc98eb91ada63e6c63ed4ec15b95598e4d082
kwanhyunkim <molba06@naver.com>
2023-11-26 23:19:29 +0900

    커맨드 기능 추가 & 테이블 세팅

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -1,5 +1,35 @@
 package com.example.demo.repository;
 
+import com.example.demo.domain.Board;
+import com.example.demo.domain.Comment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
 public class CommentRepository {
-    // to-do
+    private final EntityManager em;
+
+
+    //저장
+
+    public void save(Comment comment) {
+        em.persist(comment);
+    }
+
+    //삭제
+
+    public void deleteById(Long commentId) {
+        Comment comment = findOne(commentId);
+        em.remove(comment);
+    }
+
+    private Comment findOne(Long commentId) {
+        return em.find(Comment.class, commentId);
+    }
+
+    //수정
+    //to-do
 }
--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -1,5 +1,26 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.Board;
+import com.example.demo.domain.Comment;
+import com.example.demo.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
-    // to-do
+
+    private final CommentRepository commentRepository;
+
+    /**
+     * 커맨드 생성
+     */
+
+    @Transactional
+    public Long join(Comment comment) {
+        commentRepository.save(comment);
+        return comment.getId();
+    }
+
 }
